ed57663130ce2bf87952b1b9a1bd163972ba99e5
Runi2000 <nikola.runic2000@gmail.com>
2025-03-11 16:03:57 +0100

    Izmena main klase

--- main.py
+++ main.py
@@ -4,17 +4,9 @@ from product_manager import ProductManager
 manager = ProductManager()
 
 products = [
-    Product("TV", 539.99, 43),
-    Product("Slušalice", 5.60, 20),
-    Product("Miš", 15, 54),
-    Product("Tastatura", 24.30, 50),
-    Product("Računar", 850, 10)
+    Product("Vešmašina", 539.99, 43),
+    Product("Rukavice", 5.60, 20),
+    Product("Saksija", 15, 54),
+    Product("Čizme", 24.30, 50),
+    Product("Kosilica", 850, 10)
 ]
-
-for product in  products:
-    manager.add_products(product)
-
-manager.display_products()
-
-total_value = manager.total_value()
-print(f"Ukupna vrednost proizvoda: {total_value}€\n")
