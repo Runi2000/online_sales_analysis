1ffdffbc0be6c94e854f44fd1c4d6bafebd0c7e2
Runi2000 <nikola.runic2000@gmail.com>
2025-03-11 15:38:50 +0100

    Dodavanje funkcije za brisanje produkta

--- product_manager.py
+++ product_manager.py
@@ -14,3 +14,14 @@ class ProductManager:
     def total_value(self):
         total_value = sum(product.price * product.quantity for product in self.products)
         return total_value
+    
+    def remove_product(self, name):
+        product_to_remove = None
+        for product in self.products:
+            if product.name == name:
+                break
+        if product_to_remove:
+            self.products.remove(product_to_remove)
+            print(f"Proizvod {name} je uklonjen.\n")
+        else:
+            print(f"Proizvod {name} nije pronađen.\n")
